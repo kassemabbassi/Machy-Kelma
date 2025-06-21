a25cd37db8bb2bcc1ca9f04bd0f7ed14cfb51151
Mohamed Kassem Abbassi <kassemabbassi5@gmail.com>
2025-06-21 14:11:00 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>AI Word Search</title>
+        <title>Machy Kelma</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
     </html>
